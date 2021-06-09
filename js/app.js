c2263668efe9cb1e72429a5b1e339be583601380
tabishismail <tabish554@gmail.com>
2021-06-10 00:03:08 +0500

    done

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function changeImgRed() {
+    var image = document.getElementById('myImg');
+    if (image.src !="images/red.png") {
+        image.src = "images/red.png";
+    }
+}
+function changeImgYellow() {
+    var image = document.getElementById('myImg');
+    if (image.src !="images/yellow.png") {
+        image.src = "images/yellow.png";
+    }
+}
+function changeImgGreen() {
+    var image = document.getElementById('myImg');
+    if (image.src !="images/green.png") {
+        image.src = "images/green.png";
+    }
+}
